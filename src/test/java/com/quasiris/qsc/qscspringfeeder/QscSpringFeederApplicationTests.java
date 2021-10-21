9144165ec9b3630a73fbaba5bcca7e5cb7f530f8
Jan Bajaryn <jbn@quasiris.de>
2021-10-21 04:11:33 +0200

    cleanup

--- src/test/java/com/quasiris/qsc/qscspringfeeder/QscSpringFeederApplicationTests.java
+++ src/test/java/com/quasiris/qsc/qscspringfeeder/QscSpringFeederApplicationTests.java
@@ -1,10 +1,6 @@
 package com.quasiris.qsc.qscspringfeeder;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-import java.io.File;
 
 class QscSpringFeederApplicationTests {
 
